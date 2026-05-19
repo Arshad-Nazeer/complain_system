b9dfbecb6e12d2ad06d5557ac1224aab8b2ed189
Arshad Nazeer <arshadnhusain@gmail.com>
2026-05-19 11:38:03 +0530

    Fixed deployed API base URL

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://complain-system-backend.onrender.com"
+    baseURL: "https://complain-system-backend.onrender.com/api"
 });
 
 
