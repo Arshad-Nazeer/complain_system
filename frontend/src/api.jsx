36de7a48745df17e3fdfee642c0f37484fce0b57
Arshad Nazeer <arshadnhusain@gmail.com>
2026-05-19 11:36:18 +0530

    Fixed deployed API base URL

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,28 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-
-    baseURL:
-        "https://complain-system-backend.onrender.com/api"
-
+    baseURL: "https://complain-system-backend.onrender.com"
 });
 
 
 // ADD TOKEN AUTOMATICALLY
 API.interceptors.request.use((req) => {
 
-    const token =
-        localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
     if (token) {
-
-        req.headers.Authorization =
-            token;
-
+        req.headers.Authorization = token;
     }
 
     return req;
-
 });
 
 export default API;
